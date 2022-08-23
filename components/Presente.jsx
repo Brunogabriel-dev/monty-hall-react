31367b201e58bce249d0dcdb45e1f8386e2c39aa
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-08-22 21:27:58 -0300

    Inclusão de texto

--- components/Presente.jsx
+++ components/Presente.jsx
@@ -5,6 +5,8 @@ export default function Presente() {
     <div className={styles.presente}>
       <div className={styles.tampa}></div>
       <div className={styles.corpo}></div>
+      <div className={styles.laco1}></div>
+      <div className={styles.laco2}></div>
     </div>
   )
 }

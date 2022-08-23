170cd9314357307def7feb3618c1ec609a13b805
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-08-22 21:16:17 -0300

    Inclusão de texto

--- components/Presente.jsx
+++ components/Presente.jsx
@@ -3,7 +3,8 @@ import styles from "../styles/Presente.module.css"
 export default function Presente() {
   return (
     <div className={styles.presente}>
-
+      <div className={styles.tampa}>Tampa</div>
+      <div className={styles.corpo}>Corpo</div>
     </div>
   )
 }

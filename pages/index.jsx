f093b0745824ee020b2ac8482a813976d98aa497
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-08-22 23:00:07 -0300

    Inclusão de texto

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,8 @@ import Porta from "../components/Porta";
 
 export default function Home() {
   return (
-    <div>
+    <div style={{display: "flex"}}>
+      <Porta />
       <Porta />
     </div>
   )

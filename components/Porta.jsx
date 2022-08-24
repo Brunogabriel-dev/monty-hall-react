4b2e9f3db17dfd2a09aae72f1a1ea0040171a7bd
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-08-23 22:58:04 -0300

    Inclusão de texto

--- components/Porta.jsx
+++ components/Porta.jsx
@@ -1,9 +1,10 @@
 import styles from "../styles/Porta.module.css"
 
 export default function Porta(props) {
+  const selecionada = props.selecionada ? styles.selecionada : ''
   return (
     <div className={styles.area}>
-      <div className={`${styles.estrutura} ${styles.selecionada}`}>
+      <div className={`${styles.estrutura} ${selecionada}`}>
         <div className={styles.porta}>
           <div className={styles.numero}>3</div>
           <div className={styles.macaneta}></div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import Porta from "../components/Porta";
 export default function Home() {
   return (
     <div style={{display: "flex"}}>
-      <Porta />
+      <Porta selecionada/>
       <Porta />
     </div>
   )

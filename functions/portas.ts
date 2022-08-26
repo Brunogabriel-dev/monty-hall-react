ef5939c3889865ad48de13ec91f0e6c05f693c23
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-08-26 20:33:35 -0300

    Inclusão de texto

--- functions/portas.ts
+++ functions/portas.ts
@@ -1,11 +1,11 @@
 import PortaModel from "../model/porta";
 
-export function criarPortas(qtde: number, selecionada: number ): PortaModel []
-  return Array.from ({ length: qtde }, (_, i) => {
+export function criarPortas(qtde: number, portaComPresente: number): PortaModel[] {
+  return Array.from({ length: qtde }, (_, i) => {
     const numero = i + 1
-    const temPresente = numero === selecionada
-    return new PortaModel (numero, temPresente)
+    const temPresente = numero === portaComPresente
+    return new PortaModel(numero, temPresente)
   })
+}
 
-
-  export function atualizarPortas(portas: PortaModel[], portaModificada: PortaModel)
+export function atualizarPortas(portas: PortaModel[], portaModificada: PortaModel) { }

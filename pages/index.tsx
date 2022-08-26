54675777992525bded173ddfb1c9d1944b8ae96b
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-08-25 23:18:21 -0300

    Inclusão de texto

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
+import { useState } from "react"
 import Porta from "../components/Porta";
 import { criarPortas } from "../functions/portas"
 import PortaModel from "../model/porta";
 
 export default function Home() {
   const [p1, setP1] = useState(new PortaModel(1))
+  const [p2, setP2] = useState(new PortaModel(2))
+  const [p3, setP3] = useState(new PortaModel(3))
   
   return (
     <div style={{display: "flex", flexDirection: "column"}}>
       <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
+      <Porta value={p2} onChange={novaPorta => setP2(novaPorta)}/>
+      <Porta value={p3} onChange={novaPorta => setP3(novaPorta)}/>
     </div>
   )
 }
